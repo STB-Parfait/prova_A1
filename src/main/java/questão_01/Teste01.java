1f424b1820d87366740355d7448da785cef7f42c
STB-Parfait <jeserchivekalt@gmail.com>
2025-11-14 19:56:30 -0300

    justificativas

--- src/main/java/questão_01/Teste01.java
+++ src/main/java/questão_01/Teste01.java
@@ -3,6 +3,10 @@ package questão_01;
 public class Teste01 {
     public static void main(String[] args) {
 
+        //O padrão factory foi usado pela necessidade
+        //de poder gerar vários relatórios diferentes
+        //podendo escalar fácilmente
+
         iFactory fabricaDiario = new FactoryDiario();
         iFactory fabricaSemanal = new FactorySemanal();
 
--- src/main/java/questão_02/Teste02.java
+++ src/main/java/questão_02/Teste02.java
@@ -3,6 +3,10 @@ package questão_02;
 public class Teste02 {
     public static void main(String[] args) {
 
+        //Foi usado o padrão estrategy pela necessidade
+        //do sistema de poder alternar entre diferentes
+        //estrtégias
+
         Context contexto = new Context(new InvestidorAgressivo());
         //contexto.setEstrategia(new InvestidorConservador());
 
